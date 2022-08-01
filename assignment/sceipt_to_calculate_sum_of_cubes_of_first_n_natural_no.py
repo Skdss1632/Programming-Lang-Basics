01d153b39ffb21c6cc6d0d42216a69ac66f03463
git <skdss1632@gmail.com>
2022-08-01 11:03:58 -0700

    fixed a mistake

--- assignment/sceipt_to_calculate_sum_of_cubes_of_first_n_natural_no.py
+++ assignment/sceipt_to_calculate_sum_of_cubes_of_first_n_natural_no.py
@@ -2,6 +2,6 @@ n = int(input("enter a natural number:"))
 total = 0
 i = 1
 while i <= n:
-    total = total + i * i * i
+    total = total + (i * i * i)
     i += 1
 print("sum of cube of %d is %d" % (n, total))
