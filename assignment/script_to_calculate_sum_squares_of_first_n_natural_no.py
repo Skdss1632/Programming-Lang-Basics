295139680a40fb00fa09a69f1a2561f0566b6d64
git <skdss1632@gmail.com>
2022-08-01 10:52:40 -0700

    fixed a mistake

--- assignment/script_to_calculate_sum_squares_of_first_n_natural_no.py
+++ assignment/script_to_calculate_sum_squares_of_first_n_natural_no.py
@@ -2,6 +2,6 @@ n = int(input("enter a natural number:"))
 total = 0
 i = 1
 while i <= n:
-    total = total + i * i
+    total = total + (i * i)
     i += 1
-print("squares of %d is %d" % (n, total))
+print("sum of squares of %d is %d" % (n, total))
