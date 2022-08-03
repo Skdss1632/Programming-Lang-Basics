5a86eeb8b7cc377f4662f955aab5a5ae92e6bf3e
git <skdss1632@gmail.com>
2022-08-03 05:36:20 -0700

    changes variable names.

--- assignment/script_to_print_all_prime_no_under_100.py
+++ assignment/script_to_print_all_prime_no_under_100.py
@@ -1,7 +1,7 @@
-for i in range(2, 101):
+for n in range(2, 101):
     prime = True
-    for j in range(2, i):
-        if i % j == 0:
+    for i in range(2, n):
+        if n % i == 0:
             prime = False
     if prime == True:
-       print(i)
+       print(n)
