1cb24a2e0a3ea3466192483ace51289aca546638
git <skdss1632@gmail.com>
2022-08-03 17:06:23 -0700

    python script to print all prime number under 100 with while loop and another approach.

---
+++ assignment/(4)script_to_print_all_prime_no_under_100.py
@@ -0,0 +1,11 @@
+N = 1
+while N <= 100:
+    count = 0
+    i = 1
+    while i <= N:
+        if N % i == 0:
+            count += 1
+        i = i + 1
+    if count == 2 and N > 1:
+        print(" %d" % N, end='  ')
+    N += 1
