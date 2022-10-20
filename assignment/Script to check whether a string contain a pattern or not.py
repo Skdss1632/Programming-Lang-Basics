359e65873dc7190dd2920447091cad5726371cd6
Saurav <skdss1632@gmail.com>
2022-10-20 13:53:32 +0530

    initial commit

---
+++ assignment/Script to check whether a string contain a pattern or not.py
@@ -0,0 +1,8 @@
+# Script to check whether a string contain a pattern or not.
+
+str=input("enter a string:-")
+p=input("enter a pattern:-")
+if p in str:
+	print("yes given string contain a given pattern.")
+else:
+		print("no given string not conatin a given pattern.")
---
+++ assignment/Script to create single list from a list of strings.py
@@ -0,0 +1,5 @@
+# Script to create single list from a list of strings.
+
+l1=eval(input("enter a string type list"))
+str=' '.join(l1)
+print("single string is:-",str)
