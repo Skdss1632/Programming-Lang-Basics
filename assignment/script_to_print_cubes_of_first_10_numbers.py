1598a083e3578ffd9459b6c0bf0d02079f0e081b
sourav kumar <skdss1632@gmail.com>
2022-10-26 00:40:59 +0530

    rename file and add comments

--- assignment/script_to_print_cubes_of_first_10_numbers.py
+++
@@ -1,5 +0,0 @@
-i = 1
-while i <= 10:
-    cube = i * i * i
-    print(cube)
-    i+= 1
--- assignment/script_to_print_first_n_even_natural_no.py
+++
@@ -1,5 +0,0 @@
-n = int(input("enter a natural number:"))
-i = 1
-while i <= n:
-    print(2*i, end = ' ')
-    i+=1
