12b27261ad12ac9554cedc77a369094249e8e8de
sourav kumar <skdss1632@gmail.com>
2022-10-25 20:49:31 +0530

    rename files

--- assignment/Script to count given character in a given string..py
+++
@@ -1,6 +0,0 @@
-# Script to count given character in a given string..py
-
-str=input("enter a string:-")
-ch=input("enter any  character to count frequency:-")
-f=str.count(ch)
-print("frequency of",ch,"is:-",f)
