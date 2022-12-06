9318fb2733b7d7d50868d553c8fc3a063c63a998
sourav kumar <skdss1632@gmail.com>
2022-12-06 07:54:51 +0530

    initial commit on recursion

--- assignment/(a.21#2)print_first_n_natural_numbers_using_recursion.py
+++ assignment/(a.21#2)print_first_n_natural_numbers_using_recursion.py
@@ -2,7 +2,7 @@
 def n_natural(n):
     if n == 0:
         return 0
-    numbers = n+n_natural(n - 1)
+    numbers = n + n_natural(n - 1)
     print("first n natural numbers are:-", numbers)
     return 0
 
