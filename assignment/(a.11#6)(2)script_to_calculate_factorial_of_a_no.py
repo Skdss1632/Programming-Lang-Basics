16436b72e068d77ae2114fe7b5d3848322ccd1ac
sourav kumar <skdss1632@gmail.com>
2022-12-25 19:51:25 +0530

    initial commit on loop

--- assignment/(a.11#6)(2)script_to_calculate_factorial_of_a_no.py
+++ assignment/(a.11#6)(2)script_to_calculate_factorial_of_a_no.py
@@ -1,6 +1,6 @@
 # calculate factorial of a number.
-total = 1
+total = 0
 n = int(input("enter a natural number:"))
 for i in range(1, n+1):
-    total = total * i
+    total = total + n * n - 1
 print(total)
