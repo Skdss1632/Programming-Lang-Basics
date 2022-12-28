b1fe02cefc3f06c01b7da7bbb77062452e494366
sourav kumar <skdss1632@gmail.com>
2022-12-29 03:49:33 +0530

    initial commit on classes and object

--- assignment/(a.22#2)define_a_class_time_to_represent_time.py
+++ assignment/(a.22#2)define_a_class_time_to_represent_time.py
@@ -1,20 +1,22 @@
 # define a class time to represent time like 3hr 45min 20sec.
 class Time:
+    
     def __init__(self):
         self.hrs = 00
         self.min = 00
         self.sec = 00
 
-    def set_hrs(self):
+    def set_time(self):
         self.hrs = 4
         self.min = 55
         self.sec = 60
 
+    @property
     def display(self):
-        print(self.hrs, "hrs", self.min, "min", self.sec, "sec")
+        return self.hrs, "hrs", self.min, "min", self.sec, "sec"
 
 
-t1 = Time()
-t1.set_hrs()
-t1.display()
+clock1 = Time()
+clock1.set_time()
+print(clock1.display)
 
