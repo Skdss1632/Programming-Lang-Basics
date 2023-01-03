bb59f4e3ee20ef6032c5260e17c28fcd50242582
sourav kumar <skdss1632@gmail.com>
2023-01-03 07:55:53 +0530

    changes in loop

--- assignment/(a.23#4)printing_problem_lecture_notes.py
+++ assignment/(a.23#4)printing_problem_lecture_notes.py
@@ -1,6 +1,6 @@
-# for i in range(1, 3):
-#     print('if i is:-', i)
-#     for j in range(1, 3):
-#         print('j is', j)
-#         for k in range(10, 13):
-#             print('k is:-', k)
+for i in range(1, 4):
+    print('if i is:-', i)
+    for j in range(1, 4):
+        print('j is', j)
+        for k in range(1, i+1):
+            print('k is:-', k)
