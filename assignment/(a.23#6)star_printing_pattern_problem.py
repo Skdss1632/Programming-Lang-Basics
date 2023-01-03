c16dc870c399e265f5f0c7e2264969bdf686db73
sourav kumar <skdss1632@gmail.com>
2023-01-03 22:07:05 +0530

    initial commit on star printing problem

--- assignment/(a.23#6)star_printing_pattern_problem.py
+++ assignment/(a.23#6)star_printing_pattern_problem.py
@@ -5,6 +5,7 @@
 # * * *
 # * *
 # *
+# using for loop
 n = int(input('enter a number:-'))
 j = n
 for i in range(1, n+1):
@@ -12,3 +13,13 @@ for i in range(1, n+1):
     for k in range(1, j+2):
         print('*', end=' ')
     print()
+# using while loop
+# n = int(input('enter a number:--'))
+# i = 1
+# while i <= n:
+#     j = 1
+#     while j <= n-i+1:
+#         print('*', end=' ')
+#         j += 1
+#     print()
+#     i += 1
