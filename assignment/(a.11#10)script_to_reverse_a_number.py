a0c25dbd488821ce7606e186f30d9344c22ce889
sourav kumar <skdss1632@gmail.com>
2023-01-13 00:51:37 +0530

    initial commit

--- assignment/(a.11#10)script_to_reverse_a_number.py
+++ assignment/(a.11#10)script_to_reverse_a_number.py
@@ -1,7 +1,6 @@
-# script to reverse a number.
+# script to reverse a number
 x = int(input("enter a  number:"))
-reverse = 0
 while x > 0:
-    reverse = reverse + x % 10
+    reverse = x % 10
     x = x//10
-print(reverse, end='')
+    print(reverse, end='')
