a3342cd6e9d07dd2aba9dcc8b887cdd7d73dbab8
sourav kumar <skdss1632@gmail.com>
2023-01-20 04:05:28 +0530

    examples of abstract method

---
+++ leetcodeSolution/1.abstract method.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    # if we use abstractmethod and whatever fun has defined just below the absmthd
+    # then we should also def exactly that name fun in another class"""
+    @abstractmethod
+    def printarea(self):
+        return 0
+
+
+class Rectangle(Shape):
+    type = 'Rectangle'
+    sides = 4
+
+    def __init__(self):
+        self.length = 6
+        self.breadth = 7
+    #
+    # def printarea(self):
+    #     return self.length * self.breadth
+
+
+rect1 = Rectangle()
