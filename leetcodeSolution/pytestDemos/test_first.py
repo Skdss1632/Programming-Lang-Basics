ee8b8860f733dbda3adc07a16ebb96a0fba2d3bc
sourav kumar <skdss1632@gmail.com>
2023-01-25 09:54:26 +0530

    pytest basic examples

---
+++ leetcodeSolution/pytestDemos/test_first.py
@@ -0,0 +1,11 @@
+def test_1():
+    x = 10
+    y = 10
+    assert x == y
+
+
+def test_2():
+    name = 'selenium'
+    title = 'selenium for the web automation'
+    assert name in title
+
---
+++ leetcodeSolution/pytestDemos/test_second.py
@@ -0,0 +1,11 @@
+def test_login():
+
+    print('login to application')
+
+
+def test_checkout():
+    print('checkout')
+
+
+def test_logout():
+    print('logout from application')
