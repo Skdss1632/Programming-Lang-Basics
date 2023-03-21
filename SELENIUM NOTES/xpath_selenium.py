2aa9ee0a526801ff11e41897e8f1909232135c42
BizCompliancer <103319767+BizCompliancer@users.noreply.github.com>
2023-03-21 06:27:26 +0530

    initial commit

--- SELENIUM NOTES/xpath_selenium.py
+++ SELENIUM NOTES/xpath_selenium.py
@@ -11,17 +11,34 @@
 # driver.find_element(By.XPATH, '//*[@id="nav-xshop"]/a[1]').click()
 # time.sleep(10)
 # ......................................................................................................................
-
+import pytest
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(r'C:\drivers of browser\chromedriver_win32\chromedriver.exe')
-driver.get("https://automationexercise.com/")
-driver.maximize_window()
-time.sleep(5)
-driver.find_element(By.XPATH, '//*[@id="header"]/div/div/div/div[2]/div/ul/li[7]/a').click()
-time.sleep(5)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome(r'C:\drivers of browser\chromedriver_win32\chromedriver.exe')
+    yield driver
+
+def load_web_project(driver):
+    driver.get("https://leetcode.com/problemset/all/?sorting=W3sic29"
+               "ydE9yZGVyIjoiQVNDRU5ESU5HIiwib3JkZXJCeSI6IkRJRkZJQ1VMVFkifV0%3D&page=2")
+    driver.maximize_window()
+    time.sleep(5)
+
+def test_first(driver):
+    load_web_project(driver)
+    driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[2]/div[1]/di'
+                                  'v[1]/div[6]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/a').click()
+    time.sleep(15)
+    driver.find_element(By.CLASS_NAME, 'whitespace-nowrap').click()
+    time.sleep(4)
+
+
+
 
 
 
