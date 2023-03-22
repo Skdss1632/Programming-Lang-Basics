cd1ec6e6642faf462d3298e62bda03048649164c
sourav kumar <skdss1632@gmail.com>
2023-03-23 01:06:27 +0530

    class notes of selenium

--- SELENIUM NOTES/xpath_selenium.py
+++ SELENIUM NOTES/xpath_selenium.py
@@ -13,14 +13,16 @@
 # ......................................................................................................................
 
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(r'C:\drivers of browser\chromedriver_win32\chromedriver.exe')
+
+driver = webdriver.Chrome(r'"C:\Users\91707\Downloads\edgedriver_win64\msedgedriver.exe"')
 driver.get("https://automationexercise.com/")
+driver.implicitly_wait(15)
 driver.maximize_window()
-time.sleep(5)
-driver.find_element(By.XPATH, '//*[@id="header"]/div/div/div/div[2]/div/ul/li[7]/a').click()
+# scrolling the web page
+python = driver.find_element(By.XPATH, "//h2[normalize-space()='recommended items']")
+driver.execute_script('arguments[0].scrollIntoView(true)', python)
 time.sleep(5)
 
 
