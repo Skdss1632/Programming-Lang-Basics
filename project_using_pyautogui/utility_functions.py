8c65fb6eeb137b6bee5101805680f81e42494bfd
intern <skdss1632@gmail.com>
2024-04-18 18:50:01 +0530

    initial commit

--- project_using_pyautogui/utility_functions.py
+++ project_using_pyautogui/utility_functions.py
@@ -12,16 +12,12 @@ import pytesseract
 
 
 def click_on_browsers():
-    pyautogui.moveTo(74, 175)
-    # pyautogui.click(x=74, y=175)
-    pyautogui.sleep(2)
-    pyautogui.click()
+    pyautogui.click(x=35, y=69)
 
 
 def click_on_login_btn():
-    pyautogui.moveTo(543, 140)
     pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=543, y=140)
     pyautogui.sleep(1)
 
 
@@ -61,17 +57,12 @@ def open_url():
 
 
 def click_on_sinin_btn():
-    pyautogui.moveTo(639, 867)
-    pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=639, y=867)
 
 
 def click_on_available_btn():
-    pyautogui.moveTo(651, 603)
     pyautogui.sleep(1)
-    pyautogui.click()
-
-
+    pyautogui.click(x=651, y=603)
 
 
 def click_on_book_now():
@@ -79,44 +70,42 @@ def click_on_book_now():
 
 
 def click_on_passenger_name_input_fld():
-    pyautogui.click(x=214, y=737)
     pyautogui.sleep(1)
+    pyautogui.click(x=214, y=737)
+
 
 
 def select_passenger_name():
+    pyautogui.sleep(1)
     pyautogui.click(x=201, y=719)
-    pyautogui.sleep(1)
     pyautogui.press("shift")
-    pyautogui.sleep(1)
     pyautogui.press("down", presses=7)
-    pyautogui.sleep(1)
     pyautogui.press("shift")
     pyautogui.press("enter")
 
 
 def click_on_book_only_if_confirm_berth_are_alloted_checkbox():
-    pyautogui.moveTo(768, 413)
-    pyautogui.sleep(2)
-    pyautogui.click()
+    pyautogui.sleep(1)
+    pyautogui.click(x=768, y=413)
+
 
 
 def click_on_continue_btn_inside_passenger_details():
-    pyautogui.click(x=199, y=935)
     pyautogui.sleep(1)
+    pyautogui.click(x=199, y=935)
 
 
 def click_on_continue_btn_inside_review_journey(sleep_time_to_fill_captcha: int):
     pyautogui.sleep(sleep_time_to_fill_captcha)
-    pyautogui.moveTo(200, 516)
+    pyautogui.moveTo(x=200, y=516)
     pyautogui.sleep(1)
     pyautogui.click()
 
 
 def click_on_irctc_ewallet():
-    pyautogui.moveTo(159, 481)
     pyautogui.sleep(1)
-    pyautogui.click()
-    pyautogui.sleep(1)
+    pyautogui.click(x=159, y=481)
+
 
 
 def click_on_pay_and_book():
@@ -124,82 +113,59 @@ def click_on_pay_and_book():
 
 
 def click_on_search_btn():
-    pyautogui.moveTo(238, 690)
     pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=238, y=690)
 
 
 def input_station_name(catch_train_station_name: str, to: str):
-    pyautogui.moveTo(280, 424)
     pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=280, y=424)
     clear_input_fld()
     pyautogui.write(catch_train_station_name)
     pyautogui.press("shift")
     pyautogui.press("down")
-    pyautogui.sleep(1)
     pyautogui.press("shift")
     pyautogui.press("enter")
 
 
-    pyautogui.moveTo(250, 495)
     pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=250, y=495)
     clear_input_fld()
     pyautogui.write(to)
     pyautogui.press("shift")
     pyautogui.press("down")
-    pyautogui.sleep(1)
     pyautogui.press("shift")
     pyautogui.press("enter")
 
 
 def select_coach_type_for_booking_from_dropdown(coach_type: str):
     if coach_type == "sleeper":
-        pyautogui.moveTo(691, 489)
         pyautogui.sleep(1)
+        pyautogui.moveTo(x=691, y=489)
         pyautogui.click()
         pyautogui.press("shift")
-        pyautogui.sleep(1)
         pyautogui.press("down", presses=11)
-        pyautogui.sleep(1)
         pyautogui.press("shift")
-        pyautogui.sleep(1)
         pyautogui.press("enter")
 
     if coach_type == "ac 3 tier":
-        pyautogui.moveTo(691, 489)
-        pyautogui.sleep(2)
+        pyautogui.sleep(1)
+        pyautogui.moveTo(x=691, y=489)
         pyautogui.click()
         pyautogui.press("shift")
-        pyautogui.sleep(2)
         pyautogui.press("down", presses=7)
-        pyautogui.sleep(2)
         pyautogui.press("shift")
         pyautogui.press("enter")
 
 def select_date_for_tatakal_booking():
-    pyautogui.moveTo(764, 426)
     pyautogui.sleep(1)
-    pyautogui.click()
-    pyautogui.sleep(1)
+    pyautogui.click(x=764, y=426)
     clear_input_fld()
-    pyautogui.sleep(1)
     pyautogui.write("01/08/2024")
-    pyautogui.sleep(1)
     pyautogui.press("enter")
 
 
-
-
-    # pyautogui.click()
-    # pyautogui.sleep(2)
-    # pyautogui.moveTo(850, 647)
-    # pyautogui.click()
-    # pyautogui.sleep(2)
-
 def clear_input_fld():
-    # Press and hold Ctrl, then press A (to select all)
     pyautogui.keyDown('ctrl')
     pyautogui.press('a')
     pyautogui.keyUp('ctrl')
@@ -207,43 +173,32 @@ def clear_input_fld():
 
 
 def select_tatkal_from_dropdown():
-    pyautogui.moveTo(255, 561)
     pyautogui.sleep(1)
-    pyautogui.click()
-    pyautogui.moveTo(255, 764)
-    pyautogui.sleep(1)
-    pyautogui.click()
-
+    pyautogui.click(x=255, y=561)
+    pyautogui.click(x=255, y=764)
 
 
 def select_coach_type_for_booking(coach_type: str):
     if coach_type == "sleeper":
-        pyautogui.moveTo(617, 571)
         pyautogui.sleep(1)
-        pyautogui.click()
+        pyautogui.click(x=617, y=571)
 
 
-def input_username_and_password_of_irctc_account(username: str, password: str) -> object:
-    pyautogui.moveTo(611, 341)
+def input_username_and_password_of_irctc_account(username: str, password: str):
     pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=540, y=346)
     pyautogui.keyDown('ctrl')
     pyautogui.press('a')
     pyautogui.keyUp('ctrl')
     pyautogui.press('backspace')
     pyautogui.write(username)
-    pyautogui.sleep(1)
 
-    pyautogui.moveTo(558, 391)
-    pyautogui.sleep(1)
-    pyautogui.click()
+    pyautogui.click(x=571, y=390)
     pyautogui.keyDown('ctrl')
     pyautogui.press('a')
     pyautogui.keyUp('ctrl')
     pyautogui.press('backspace')
-    pyautogui.write(username)
-    pyautogui.sleep(1)
-
+    pyautogui.write(password)
 
 
 def scroll_to_continue_btn():
---
+++ pyautogui_basics1/prank_from_whatapp.py
@@ -0,0 +1,10 @@
+from Programming_Lang_Basics.project_using_pyautogui.utility_functions import *
+import pyautogui
+
+click_on_browsers()
+while True:
+    pyautogui.click(883, 1029)
+    pyautogui.write("pan card link kiya jldi boll")
+    pyautogui.press("enter")
+    pyautogui.sleep(5)
+
