f274a0d6e1b5647f6f0e03d61b8dbd6381cb79f9
intern <skdss1632@gmail.com>
2024-04-21 07:54:27 +0530

    initial commit

--- project_using_pyautogui/project.py
+++ project_using_pyautogui/project.py
@@ -1,55 +1,65 @@
+import schedule
 from Programming_Lang_Basics.project_using_pyautogui.utility_functions import *
 
 
-def Schedule_task_at_specific_time():
-    # open_chrome_browser_with_irctc_page()
-    # if wait_until_image_found("/home/intern/Pictures/Screenshots/login_btn.png"):
-    #     login_irctc_account(username="skdss16321", password="Sourav99#", sleep_time_to_fill_captcha=10)
-    pyautogui.sleep(3)
-    journey_details(from_="dalsinghsarai", to="samastipur", select_date_for_tatakal="01/08/2024")
+def schedule_task_at_specific_time():
+    """This program is designed to function correctly only on the Chrome browser with the page zoom set to 100%.
+    If the browser or page size is changed,
+    the program may not operate properly and also do not change the browser position on home page."""
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/sleeper_refresh.png"):
+    open_chrome_browser_with_irctc_page()
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/login_btn.png"):
+        login_irctc_account(username="skdss16321",
+                          password="Sourav99#", captcha_fill_delay=10)
+
+
+    # pyautogui.sleep(2)
+    # click_browsers()
+    input_journey_details(from_="NEW DELHI", to="SAMASTIPUR", booking_date="01/08/2024")
+
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/sleeper_refresh.png"):
         select_coach_type_for_booking(coach_type="sleeper")
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/avalible_ticket.png"):
-        click_on_available_btn()
-        click_on_book_now()
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/available_ticket.png"):
+        click_available_btn()
+        click_book_now()
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/add_infrant_without_berth.png"):
-        click_on_passenger_name_input_fld()
-        select_passenger_name()
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/add_infant_without_berth.png"):
+        click_passenger_name_fld()
+        select_passenger_name(input_passenger_name_index=7)
         scroll_to_continue_btn(-7)
-        click_on_book_only_if_confirm_berth_are_alloted_checkbox()
-        click_on_continue_btn_inside_passenger_details()
+        click_book_only_if_confirm_berth_alloted()
+        click_continue_btn_inside_passenger_details()
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/view_cancellation_policy.png"):
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/view_cancellation_policy.png"):
         scroll_to_continue_btn(-6)
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/continue_btn.png"):
-        click_on_continue_btn_inside_review_journey(sleep_time_to_fill_captcha=10, image_path="/home/intern/Pictures/Screenshots/continue_btn.png")
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/continue_btn.png"):
+        click_continue_btn_inside_review_journey(captcha_fill_delay=10, image_path="/home/intern/Pictures/Screenshots/continue_btn.png")
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/irctc_ewallet.png"):
-        click_on_irctc_ewallet()
-        click_and_pay()
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/irctc_ewallet.png"):
+        click_irctc_e_wallet()
+        click_pay_and_book()
 
-    if wait_until_image_found("/home/intern/Pictures/Screenshots/confirm_btn.png"):
-        click_on_confirm_btn_inside_otp(sleep_time_to_fill_otp=5)
-
-
-Schedule_task_at_specific_time()
+    if wait_until_image_visible("/home/intern/Pictures/Screenshots/confirm_btn.png"):
+        click_confirm_btn_inside_otp(otp_fill_delay=10)
 
 
+# # Schedule the task to run at 11:00 AM every day
+# schedule.every().day.at("15:56").do(Schedule_task_at_specific_time)
 
-
-# Schedule the task to run at 11:00 AM every day
-# schedule.every().day.at("03:44").do(my_task)
-
-# # Run the scheduler
 # while True:
 #     schedule.run_pending()
-#     pyautogui.sleep(1)  # Sleep for 1 second to avoid high CPU usage
 
-# my_task()
+
+schedule_task_at_specific_time()
+
+
+
+
+
+
+
 
 
 
--- project_using_pyautogui/utility_functions.py
+++ project_using_pyautogui/utility_functions.py
@@ -6,9 +6,7 @@ import pytesseract
 # import datetime
 
 
-
-
-def click_on_browsers():
+def click_browsers():
     pyautogui.click(x=484, y=1045)
 
 
@@ -27,12 +25,9 @@ def open_url():
     # pyautogui.press('enter')
     # pyautogui.sleep(1)
     url = "https://www.irctc.co.in/nget/train-search"
-
-    # Iterate through each character in the URL and type it slowly
     for char in url:
         pyautogui.write(char)
     pyautogui.press("enter")
-
 
 
 # def get_image_txt():
@@ -49,54 +44,49 @@ def open_url():
 #     print(text)
 
 
-
 def click_on_sinin_btn():
     pyautogui.press("enter")
 
 
-def click_on_available_btn():
+def click_available_btn():
     pyautogui.click(x=651, y=603)
 
 
-def click_on_book_now():
+def click_book_now():
     pyautogui.click(x=546, y=711)
 
 
-def click_on_passenger_name_input_fld():
+def click_passenger_name_fld():
     pyautogui.click(x=214, y=737)
 
 
-def select_passenger_name():
+def select_passenger_name(input_passenger_name_index: int):
     pyautogui.click(x=201, y=719)
     pyautogui.press("shift")
-    pyautogui.press("down", presses=7)
+    pyautogui.press("down", presses=input_passenger_name_index)
     pyautogui.press("shift")
     pyautogui.press("enter")
 
 
-def click_on_book_only_if_confirm_berth_are_alloted_checkbox():
+def click_book_only_if_confirm_berth_alloted():
     pyautogui.click(x=727, y=374)
 
 
-def click_on_continue_btn_inside_passenger_details():
+def click_continue_btn_inside_passenger_details():
     pyautogui.click(x=137, y=888)
 
 
-def click_on_continue_btn_inside_review_journey(sleep_time_to_fill_captcha: int, image_path: str):
-    pyautogui.sleep(sleep_time_to_fill_captcha)
+def click_continue_btn_inside_review_journey(captcha_fill_delay: int, image_path: str):
+    pyautogui.sleep(captcha_fill_delay)
     button_location = pyautogui.locateCenterOnScreen(image_path)
     pyautogui.click(button_location)
 
 
-def click_on_irctc_ewallet():
+def click_irctc_e_wallet():
     pyautogui.click(x=159, y=481)
 
 
-def click_on_pay_and_book():
-    pyautogui.click(x=647, y=548)
-
-
-def click_on_search_btn():
+def click_search_btn():
     pyautogui.click(x=238, y=690)
 
 
@@ -109,7 +99,7 @@ def input_station_name(from_: str, to: str):
     pyautogui.press("down")
     pyautogui.press("shift")
     pyautogui.press("enter")
-
+    # pyautogui.hotkey('shift', 'down', 'shift', 'enter')
 
     pyautogui.sleep(1)
     pyautogui.click(x=250, y=495)
@@ -121,10 +111,10 @@ def input_station_name(from_: str, to: str):
     pyautogui.press("enter")
 
 
-def select_date_for_tatakal_booking(select_date_for_tatakal: str):
+def tatkal_booking_date(tatkal_booking_date: str):
     pyautogui.click(x=764, y=426)
     clear_input_fld()
-    pyautogui.write(select_date_for_tatakal)
+    pyautogui.write(tatkal_booking_date)
     pyautogui.press("enter")
 
 
@@ -144,38 +134,44 @@ def select_coach_type_for_booking(coach_type: str):
         pyautogui.click(x=617, y=571)
 
 
-def input_username_and_password_of_irctc_account(username: str, password: str):
-    pyautogui.sleep(2)
-    pyautogui.click(x=537, y=317)
-    pyautogui.sleep(2)
+def input_username_irctc_account(username: str, username_image_path: str):
+    username_fld = pyautogui.locateCenterOnScreen(username_image_path)
+    pyautogui.click(username_fld)
     pyautogui.hotkey("ctrl", "a")
     pyautogui.press('backspace')
     pyautogui.write(username)
 
-    pyautogui.sleep(1)
-    pyautogui.click(x=514, y=361)
+
+def input_password_irctc_account(password: str, password_image_path: str):
+    password_fld = pyautogui.locateCenterOnScreen(password_image_path)
+    pyautogui.click(password_fld)
     pyautogui.hotkey("ctrl", "a")
     pyautogui.press('backspace')
     pyautogui.write(password)
 
 
-def login_irctc_account(username: str, password: str, sleep_time_to_fill_captcha: int):
+def login_irctc_account(username: str, password: str, captcha_fill_delay: int):
     click_on_login_btn()
-    # if wait_until_image_found("/home/intern/Pictures/Screenshots/ad_on_login_popup.png"):
-    input_username_and_password_of_irctc_account(username=username, password=password)
-    pyautogui.sleep(sleep_time_to_fill_captcha)
+    pyautogui.sleep(2)
+    if wait_until_image_visible(image_path="/home/intern/Pictures/Screenshots/username_fld.png"):
+        input_username_irctc_account(username=username, username_image_path="/home/intern/Pictures/Screenshots/username_fld.png")
+
+    if wait_until_image_visible(image_path="/home/intern/Pictures/Screenshots/password_fld.png"):
+        input_password_irctc_account(password=password, password_image_path="/home/intern/Pictures/Screenshots/password_fld.png")
+
+    pyautogui.sleep(captcha_fill_delay)
     click_on_sinin_btn()
 
 
-def journey_details(from_: str, to: str, select_date_for_tatakal: str):
+def input_journey_details(from_: str, to: str, booking_date: str):
     input_station_name(from_=from_, to=to)
     # select_tatkal_from_dropdown()
-    select_date_for_tatakal_booking(select_date_for_tatakal=select_date_for_tatakal)
-    click_on_search_btn()
+    tatkal_booking_date(booking_date)
+    click_search_btn()
 
 
 def open_chrome_browser_with_irctc_page():
-    click_on_browsers()
+    click_browsers()
     open_new_tab()
     open_url()
 
@@ -184,11 +180,11 @@ def scroll_to_continue_btn(no_of_scroll: float):
     pyautogui.scroll(no_of_scroll)
 
 
-def click_and_pay():
+def click_pay_and_book():
     pyautogui.click(771, 883)
 
 
-def wait_until_image_found(image_path):
+def wait_until_image_visible(image_path):
     while True:
         try:
             if pyautogui.locateCenterOnScreen(image_path, confidence=0.80) is not None:
@@ -198,8 +194,8 @@ def wait_until_image_found(image_path):
             pass
 
 
-def click_on_confirm_btn_inside_otp(sleep_time_to_fill_otp: int):
-    pyautogui.sleep(sleep_time_to_fill_otp)
+def click_confirm_btn_inside_otp(otp_fill_delay: int):
+    pyautogui.sleep(otp_fill_delay)
     pyautogui.click(711, 541)
 
 
