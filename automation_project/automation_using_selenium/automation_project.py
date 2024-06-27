a057e867a7f705b6e0e5e9c007994858bf5e9422
intern <skdss1632@gmail.com>
2024-06-28 00:00:46 +0530

    automation using selenium: wip

--- automation_project/automation_using_selenium/automation_project.py
+++ automation_project/automation_using_selenium/automation_project.py
@@ -1,27 +1,29 @@
+import time
+
 import undetected_chromedriver as uc
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 # Use undetected-chromedriver to create a WebDriver instance
 options = uc.ChromeOptions()
-options.add_argument("--headless")  # Optional: Run Chrome in headless mode
+options.add_argument("--disable-notifications")
+# options.add_argument("--headless")  # Optional: Run Chrome in headless mode
 driver = uc.Chrome(options=options)
-
 # Maximize the window (optional)
 driver.maximize_window()
+actions = ActionChains(driver)
 
-# Navigate to the desired URL
-driver.get('https://help.ipfoxy.com/docs/Developer-API')
 
-try:
-    # Wait for the presence of the element with the specified ID
-    element = WebDriverWait(driver, 10).until(
-        ec.presence_of_element_located((By.ID, "element_id"))
-    )
-except TimeoutException:
-    print("Loading took too much time!")
+driver.get('https://www.irctc.co.in/nget/train-search')
+login_btn_element = WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.CLASS_NAME, "search_btn")))
+login_btn_element.click()
+time.sleep(10)
 
-# Close the WebDriver instance
+
+
+
 driver.quit()
+
